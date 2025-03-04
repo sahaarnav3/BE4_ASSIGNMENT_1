138908527a00f9bc5ece034a2367c63502c2f188
Arnav Saha <sahaarnav3@gmail.com>
2025-03-04 17:37:57 +0530

    Added route to fetch all books data from DB.

--- index.js
+++ index.js
@@ -10,20 +10,47 @@ app.use(express.json());
 
 initialiseDatabase();
 
+//Route to create a book entry.
 app.post("/books", async (req, res) => {
   try {
     const book = new Book(req.body);
     const saveBook = await book.save();
-    if(!saveBook)
-        res.status(404).json({ error: "Error occurred in saving new book. Please check data format and try again." });
+    if (!saveBook)
+      res
+        .status(404)
+        .json({
+          error:
+            "Error occurred in saving new book. Please check data format and try again.",
+        });
+    else
+      res
+        .status(200)
+        .json({
+          message: "Book entry created successfully.",
+          "Book-Data": saveBook,
+        });
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "Some error occured with the request while creating new book in the DB. Please try again.",
+    });
+  }
+});
+
+//Route to fetch all book from DB.
+app.get("/books", async (req, res) => {
+  try {
+    const bookData = await Book.find();
+    if(!bookData)
+        res.status(404).json({ error: "Either some error occured or there is no entry present in the DB." });
     else 
-        res.status(200).json({ message: "Book entry created successfully.", "Book-Data": saveBook });
+        res.status(200).json({ message: "Fetched all Book data from db.", "Book-List-Data": bookData });
   } catch (error) {
     res
       .status(500)
       .json({
         error:
-          "Some error occured with the request while creating new book in the DB. Please try again.",
+          "Some error occurred with the request while fetching all the books from DB. Please try again.",
       });
   }
 });
