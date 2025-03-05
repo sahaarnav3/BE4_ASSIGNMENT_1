2a05eeb7e692d7743bd86edc8530b3d26b0944ab
Arnav Saha <sahaarnav3@gmail.com>
2025-03-05 13:35:24 +0530

    Added all other routes and added README file with all the endpoints

--- index.js
+++ index.js
@@ -64,9 +64,77 @@ app.get("/books/:bookTitle", async (req, res) => {
     else 
       res.status(200).json({ message: "Book fetched successfully from the DB.", "Book-Data": bookData });
   } catch (error) {
-    res.status(505).json({ error: "Some error occuredd with the request while fetching book. Please try again." });
+    res.status(500).json({ error: "Some error occuredd with the request while fetching book. Please try again." });
   }
-})
+});
+
+//Fetch all books by a particular author.
+app.get("/books/author/:authorName", async (req, res) => {
+  try {
+    const bookData = await Book.find({ author: req.params.authorName });
+    if(!bookData)
+      res.status(404).json({ error: "Either the book is not present or some other error occurred. Please try again." });
+    else 
+      res.status(200).json({ message: "Book fetched successfully from the DB.", "Book-Data": bookData });
+  } catch (error) {
+    res.status(500).json({ error: "Some error occuredd with the request while fetching book. Please try again." });
+  }
+});
+
+//Fetch all books of a particular genre.
+app.get("/books/genre/:genre", async (req, res) => {
+  try {
+    const bookData = await Book.find({ genre: req.params.genre });
+    if(!bookData)
+      res.status(404).json({ error: "Either the book is not present or some other error occurred. Please try again." });
+    else 
+      res.status(200).json({ message: "Book fetched successfully from the DB.", "Book-Data": bookData });
+  } catch (error) {
+    res.status(500).json({ error: "Some error occuredd with the request while fetching book. Please try again." });
+  }
+});
+
+//Fetch all books of a particular year.
+app.get("/books/year/:publishedYear", async (req, res) => {
+  try {
+    const bookData = await Book.find({ publishedYear: req.params.publishedYear });
+    if(!bookData)
+      res.status(404).json({ error: "Either the book is not present or some other error occurred. Please try again." });
+    else 
+      res.status(200).json({ message: "Book fetched successfully from the DB.", "Book-Data": bookData });
+  } catch (error) {
+    res.status(500).json({ error: "Some error occuredd with the request while fetching book. Please try again." });
+  }
+});
+
+//Find the book by title then update the rating.
+app.post("/books/update/title/:bookTitle", async(req, res) => {
+  try {
+    const bookData = await Book.findOneAndUpdate({ title: req.params.bookTitle }, req.body, {new: true});
+    if(!bookData)
+      res.status(404).json({ error: "Book does not exist. Please try again with different value." });
+    else
+      res.status(200).json({ message: "The book data is updated.", "New-Book-Data": bookData });
+    
+  } catch (error) {
+    res.status(500).json({ error: "Some error occuredd with the request while updating book. Please try again." });
+  }
+});
+
+//Find book by id and delete from DB.
+app.delete("/books/delete/:bookId", async (req, res) => {
+  try {
+    const bookData = await Book.findOneAndDelete(req.params.bookId);
+    if(!bookData)
+      res.status(404).json({ error: "Book does not exist. Please try again with different ID value." });
+    else 
+      res.status(200).json({ message: "Book Deleted Successfully. "});
+  } catch (error) {
+    res.status(500).json({ error: "Some error occuredd with the request while deleting the book. Please try again." });
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log("Server is Running on PORT:", PORT);
